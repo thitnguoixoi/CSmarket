ba00366b88e6eb5db9e00db102439c23585b2b7b
thitnguoixoi <vanlong03062003@gmail.com>
2023-11-06 13:36:12 +0700

    init backend

---
+++ sources/backend/src/config/viewEngine.js
@@ -0,0 +1,7 @@
+import express, { application } from "express";
+let configViewEngine = (app) => {
+    app.use(express.static("./src/public"));
+    app.set("view engine", "ejs");
+    app.set("views", "./src/view");
+}
+module.exports = configViewEngine;
---
+++ sources/backend/src/route/web.js
@@ -0,0 +1,9 @@
+import express from "express";
+let router = express.Router();
+let initWebRoutes = (app) => {
+    router.get('/', (req, res) => {
+        return res.send("thitnguoixoi");
+    })
+    return app.use("/", router);
+}
+module.exports = initWebRoutes;
