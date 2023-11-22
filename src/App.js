bdf0f58101beff0d8ac6a5fb083445c2c91be86b
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-11-22 14:53:50 +0700

    freecase1_opened

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Header from './components/Header.js';
 import Body from './components/Body.js';
 import Opencase from './components/Opencase.js';
 import Footer from './components/Footer.js'
+import Case_opened from './components/Case_Opened.js';
 
 function App() {
   return (
     <div className="content">
       <Header></Header>
-      <Body></Body>
-      <Opencase></Opencase>
+      <Case_opened></Case_opened>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Case_Opened.js
@@ -0,0 +1,71 @@
+// CaseOpened.js
+import React from 'react';
+import './styles/Case_opened.css';
+
+import freecase1 from '../assets/case/freecase1.png';
+
+import tier1 from '../assets/frame/tier1.png';
+import tier2 from '../assets/frame/tier2.png';
+import tier3 from '../assets/frame/tier3.png';
+import tier4 from '../assets/frame/tier4.png';
+import tier5 from '../assets/frame/tier5.png';
+
+import tier1_1 from '../assets/skin/Freecase1/tier1/tier1_1.png';
+import tier1_2 from '../assets/skin/Freecase1/tier1/tier1_2.png';
+
+import tier2_1 from '../assets/skin/Freecase1/tier2/tier2_1.png';
+import tier2_2 from '../assets/skin/Freecase1/tier2/tier2_2.png';
+import tier2_3 from '../assets/skin/Freecase1/tier2/tier2_3.png';
+
+import tier3_1 from '../assets/skin/Freecase1/tier3/tier3_1.png';
+import tier3_2 from '../assets/skin/Freecase1/tier3/tier3_2.png';
+import tier3_3 from '../assets/skin/Freecase1/tier3/tier3_3.png';
+
+import tier4_1 from '../assets/skin/Freecase1/tier4/tier4_1.png';
+import tier4_2 from '../assets/skin/Freecase1/tier4/tier4_2.png';
+
+import tier5_1 from '../assets/skin/Freecase1/tier5/tier5_1.png';
+import tier5_2 from '../assets/skin/Freecase1/tier5/tier5_2.png';
+// SkinTier component defined inside CaseOpened.js
+const SkinTier = ({ tier, images }) => (
+  <div className={`tier${tier}`}>
+    {images.map((image, index) => (
+      <img key={index} src={image}/>
+    ))}
+  </div>
+);
+
+function Case_Opened(){
+    return(
+        <>
+            <div className="opencase" id='free1'>
+                <h1>Freecase1</h1>
+                <img src={freecase1} alt="" />
+                <button>Open $0.00</button>
+            </div>
+
+            <div className="case_item">
+                <div className="image-container">
+                    <img src={tier1_1} className="tier1"/>
+                    <img src={tier1_2} className="tier1"/>
+                    
+                    <img src={tier2_1} className="tier2"/>
+                    <img src={tier2_2} className="tier2"/>
+                    <img src={tier2_3} className="tier2"/>
+                    
+                    <img src={tier3_1} className="tier3"/>
+                    <img src={tier3_2} className="tier3"/>
+                    <img src={tier3_3} className="tier3"/>
+
+                    <img src={tier4_1} className="tier4"/>
+                    <img src={tier4_2} className="tier4"/>
+
+                    <img src={tier5_1} className="tier5"/>
+                    <img src={tier5_2} className="tier5"/>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Case_Opened;
