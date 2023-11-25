22ed3461e43a45cfad7e7a35d719fcdd56021108
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-11-25 15:13:49 +0700

    fix viền trắng

---
+++ src/components/Upgrade.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles/Upgrade.css';
+class Upgrade extends React.Component {
+    render() {
+        return (
+            <>
+                
+            </>
+        );
+
+
+
+    }
+}
+
+export default Upgrade;
