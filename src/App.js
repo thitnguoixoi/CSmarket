5a1788a20fa292d1a5ed086c71ccb3ab75213071
Chocopyke <Vu.tuanson159@gmail.com>
2023-11-29 13:16:37 +0700

    test router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Header from './components/Header.js';
 import Body from './components/Body.js';
 import Opencase from './components/Opencase.js';
 import Footer from './components/Footer.js'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 function App() {
   return (
     <div className="content">
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,24 @@
 import './styles/Body.css';
 import {
-    BrowserRouter as Router,
+    Routes,
     Switch,
     Route,
-    Link
-} from "react-router-dom";
+    Link,
+} from 'react-router-dom';
 import Slider from './Slider.js';
 import Opencase from './Opencase.js';
 import FC1 from './cases/FC1/FC1.js';
 import FC2 from './cases/FC2/FC2.js';
 import CC1 from './cases/CC1/CC1.js';
 import Inventory from './Upgrade/Upgrade.js';
+
 function Body(){
     return(
         <>
-            <Inventory />
+            <Routes>
+                <Route path='/home' exact element={<Opencase/>}></Route>
+                <Route path='/Upgrade' exact component={Inventory}></Route>
+            </Routes>
         </>
     )
 }
