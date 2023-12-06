58bd882e4d40292e9520a97171d1a8d7cbd263e8
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-12-06 13:00:22 +0700

    login but

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Header.css';
 import logo from '../assets/logo/csmarket.png';
 
 function Header() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleLogin = () => {
+    // Add your login logic here
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    // Add your logout logic here
+    setLoggedIn(false);
+  };
+
+  const handleAvatarClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div className="header">
       <Link to="/">
@@ -13,7 +30,24 @@ function Header() {
         <a href="Opencase">Opencase</a>
         <a href="Upgrade">Upgrade</a>
       </div>
-      <button>Login via steam</button>
+      {isLoggedIn ? (
+        <div className="avatar-container" onClick={handleAvatarClick}>
+          <img
+            className="avatar"
+            src="path/to/avatar.jpg" // Add the path to the user's avatar
+            alt="User Avatar"
+          />
+          {showDropdown && (
+            <ul className="dropdown-menu">
+              <li>Option 1</li>
+              <li>Option 2</li>
+              <li onClick={handleLogout}>Logout</li>
+            </ul>
+          )}
+        </div>
+      ) : (
+        <button onClick={handleLogin}>Login via steam</button>
+      )}
     </div>
   );
 }
