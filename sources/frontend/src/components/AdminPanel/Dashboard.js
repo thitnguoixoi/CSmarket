2be4cc8d08c13fff8296c8be236d0e576fa08c9e
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-12-14 00:21:38 +0700

    search for table

--- sources/frontend/src/components/AdminPanel/Dashboard.js
+++ sources/frontend/src/components/AdminPanel/Dashboard.js
@@ -1,73 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Dashboard() {
-  // Your array of recent activities
-  const recentActivities = [
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    { content: "User John Doe registered.", time: "10 minutes ago" },
-    { content: "Case #123 created.", time: "20 minutes ago" },
-    // Add other activities as needed
-  ];
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch your data or set it statically
+    // Example data:
+    const recentActivities = [
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      { content: "User John Doe registered.", time: "10 minutes ago" },
+      { content: "Case #123 created.", time: "20 minutes ago" },
+      // Add other activities as needed
+    ];
+
+    setData(recentActivities);
+    setFilteredData(recentActivities);
+  }, []);
+
+  const handleSearch = (e) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+
+    const filtered = data.filter((item) =>
+      Object.values(item)
+        .join(' ')
+        .toLowerCase()
+        .includes(term)
+    );
+
+    setFilteredData(filtered);
+    setCurrentPage(1);
+  };
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const renderTable = () => (
+    <table>
+      <thead>
+        <tr>
+          <th>Content</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {currentItems.map((item) => (
+          <tr key={item.id}>
+            <td>{item.content}</td>
+            <td>{item.time}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const renderPagination = () => {
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+    if (totalPages <= 1) {
+      return null;
+    }
+
+    return (
+      <div className="page-nums">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <span
+            key={index}
+            onClick={() => paginate(index + 1)}
+            style={{
+              cursor: 'pointer',
+              margin: '0 5px',
+              fontWeight: currentPage === index + 1 ? 'bold' : 'normal',
+            }}
+          >
+            {index + 1}
+          </span>
+        ))}
+      </div>
+    );
+  };
 
   return (
-    <>
-      <div className="dashboard">
-        <h2>Admin Dashboard</h2>
-        <h3>Statistics</h3>
-        <div className="live-static">
-          <p>Total Users: 100</p>
-          <p>Total Cases: 50</p>
-          <p>Total Skins: 200</p>
-        </div>
-
-        <div className="recent-activities">
-          <h3>Recent Activities</h3>
-          <div className="content">
-            <div class="tableFixHead">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Content</th>
-                    <th>Time</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {recentActivities.map((activity, index) => (
-                    <tr key={index}>
-                      <td>{activity.content}</td>
-                      <td>{activity.time}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="skin-management">
+      <h2>Skin Management</h2>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      {renderTable()}
+      {renderPagination()}
+    </div>
   );
 }
 
