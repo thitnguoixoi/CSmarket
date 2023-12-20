97708e599d3ef2b1f409b08fafb63cabd26be5f5
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-12-21 01:59:32 +0700

    fix

--- sources/frontend/src/components/Header.js
+++ sources/frontend/src/components/Header.js
@@ -84,7 +84,8 @@ function Header({ user, setUser }) {
               <Link to="/panel"><li>User Profile</li></Link>
               {/* Assuming userIsAdmin is a state/prop indicating admin status */}
               {userIsAdmin && <Link to="/AdminPanel"><li>AdminPanel</li></Link>}
-              <li onClick={handleLogout}><Link to="/">Logout</Link></li>
+              <Link to="/"><li onClick={handleLogout}>Logout</li></Link>
+
             </ul>
           )}
         </div>
