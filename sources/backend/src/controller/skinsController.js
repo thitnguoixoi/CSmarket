07f531862c805c834ca7f0e8c80c1687088b0315
thitnguoixoi <vanlong03062003@gmail.com>
2023-12-23 23:10:29 +0700

    fix sell

--- sources/backend/src/controller/skinsController.js
+++ sources/backend/src/controller/skinsController.js
@@ -1,2 +1,78 @@
 import skinService from "../service/skinService"
+const readSkins = async (req, res) => {
+    try {
+        let data = await skinService.getSkins();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EM: "Error from server",
+            EC: "-1",
+            DT: ""
+        })
+    }
+}
 
+const createSkin = async (req, res) => {
+    try {
+        let data = await skinService.createaSkin();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EM: "Error from server",
+            EC: "-1",
+            DT: ""
+        })
+    }
+}
+
+const updateSkin = async (req, res) => {
+    try {
+        let data = await skinService.updateaSkin();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EM: "Error from server",
+            EC: "-1",
+            DT: ""
+        })
+    }
+}
+
+const deleteSkin = async (req, res) => {
+    try {
+        let data = await skinService.deleteaSkin();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({
+            EM: "Error from server",
+            EC: "-1",
+            DT: ""
+        })
+    }
+}
+module.exports = {
+    readSkins,
+    createSkin,
+    updateSkin,
+    deleteSkin
+}
