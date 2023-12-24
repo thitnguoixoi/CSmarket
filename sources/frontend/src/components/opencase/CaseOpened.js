f7ca111b7fad990902cfbe8c406006ac3ae1102c
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-12-24 16:52:26 +0700

    Update CaseOpened.js

--- sources/frontend/src/components/opencase/CaseOpened.js
+++ sources/frontend/src/components/opencase/CaseOpened.js
@@ -4,47 +4,50 @@ import './styles/CaseOpened.css';
 import Popup from "./popup";
 
 function CaseOpened(id) {
-    const [data, setData] = useState([]);
+    const [caseData, setCaseData] = useState([]);
+    const [skinData, setSkinData] = useState([]);
     const [buttonPopup, setButtonPopup] = useState(false);
     const handlePopup = () => {
         setButtonPopup(false);
     }
     useEffect(() => {
-        console.log(id.id);
         axios.get(`/api/v1/cases/id`, { params: { caseid: id.id } })
             .then(response => {
-                console.log(response);
+                setCaseData(response.data.DT.acase);
+                setSkinData(response.data.DT.skins);
             })
             .catch(error => {
                 console.error('Error checking user group:', error);
             });
     }, []);
+    // console.log(caseData);
+    // console.log(skinData);
     return (
         <div className="Case_Opened">
             <div className="opencase">
-                <h3>Name</h3>
-                <img src='' alt="Case" />
-                <button className="opencase-btn" onClick={() => setButtonPopup(true)}>Open </button>
-                {/* <Popup trigger={buttonPopup} setTrigger={handlePopup}>
+                <h3>{caseData.Name}</h3>
+                <img src={caseData.Image} alt="Case" />
+                <button className="opencase-btn" onClick={() => setButtonPopup(true)}>Open {caseData.Price}$ </button>
+                <Popup trigger={buttonPopup} setTrigger={handlePopup}>
                     <h3>Congratulation</h3>
                     <h4>You got</h4>
-                    <div className={`tierskin tier${caseData.skins[0].tier}`}>
+                    {/* <div className={`tierskin tier${caseData.skins[0].tier}`}>
                         <img src={caseData.skins[0].imgUrl} alt="skin" />
                         <h4>{caseData.skins[0].name}</h4>
-                    </div>
-                </Popup> */}
+                    </div> */}
+                </Popup>
             </div>
 
-            {/* <div className="image-container">
-                {caseData.skins.map((item) => {
+            <div className="image-container">
+                {skinData.map((item) => {
                     return (
-                        <div className={`tierskin tier${item.tier}`} >
-                            <img src={item.imgUrl} alt="skin" />
-                            <h4>{item.name}</h4>
+                        <div className={`tierskin tier${item.Skin.Tier}`} >
+                            <img src={item.Skin.Image} alt="skin" />
+                            <h4>{item.Skin.Name}</h4>
                         </div>
                     )
                 })}
-            </div> */}
+            </div>
         </div>
     );
 }
