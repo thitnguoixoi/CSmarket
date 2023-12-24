305bd9669748a642df17e921f9a3ffaaa0f0fbb4
thitnguoixoi <vanlong03062003@gmail.com>
2023-12-24 17:45:44 +0700

    fix api

--- sources/backend/src/middleware/jwtactions.js
+++ sources/backend/src/middleware/jwtactions.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 require("dotenv").config();
 
-const nonSecurePath = ["/auth/steam", "/auth/steam/return", "/jwt/steamid", "/cases", "/cases/update"]
+const nonSecurePath = ["/auth/steam", "/auth/steam/return", "/jwt/steamid", "/cases"]
 
 const createJWT = (payload) => {
     let key = process.env.JWT_KEY
