aa0f42db1495ce328f713539e094450ef45bb03d
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-12-25 04:04:08 +0700

    fix

--- sources/frontend/src/components/AdminPanel/SkinManagement.js
+++ sources/frontend/src/components/AdminPanel/SkinManagement.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBackward, faPlus, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBackward, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import axios from "../../assets/setup/axios"
 
 function SkinManagement() {
