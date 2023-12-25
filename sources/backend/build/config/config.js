31de514b0eace093daef8fe2d6502258627e6629
thitnguoixoi <vanlong03062003@gmail.com>
2023-12-26 03:13:09 +0700

    check

---
+++ sources/backend/build/config/config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("dotenv").config();
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "csmarket",
+    "host": "172.30.32.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "csmarket",
+    "host": "172.30.32.1",
+    "dialect": "mysql"
+  }
+};
